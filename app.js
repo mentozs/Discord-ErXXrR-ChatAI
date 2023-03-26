927074450322b4fde1b8102b137a22fadea66962
Mentozs <mentozs@hotmail.com>
2023-03-26 11:54:45 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ import { getShuffledOptions, getResult } from './game.js';
 
 // Create an express app
 const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
+// Get port, or default to 3000 (const PORT = process.env.PORT || 3000;)
+const PORT = process.env.PORT;
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
